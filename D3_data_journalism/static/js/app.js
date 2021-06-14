fa3de64edf38ee4f385452c3dda50a720f85ef42
DANIEL <daniel@la-MacBook-Pro-de-Macintosh.local>
2021-06-14 13:31:21 -0500

    Created the SVG

--- D3_data_journalism/static/js/app.js
+++ D3_data_journalism/static/js/app.js
@@ -143,3 +143,32 @@ function updateToolTip(chosenXAxis, circlesGroup) {
 
     return circlesGroup;
 }
+
+
+//########################  3.  SVG Setup ###################################//
+
+var svgWidth = 960;
+var svgHeight = 550;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 100,
+    left: 100
+};
+
+// xScale uses width so xScale() can only be called below this point
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
